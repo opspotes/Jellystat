6638f016cafd39617c2dd9781a7c6addab865954
Sylvain Blanc <sylvain.blanc@dawex.com>
2023-10-22 10:11:08 +0200

    clean dependencies

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import Config from "./lib/config";
 
 import Loading from "./pages/components/general/loading";
